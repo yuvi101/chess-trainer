cce22351ad7be39d3ac516eea17b96eba185587a
yuval2014g <yuval2014g@gmail.com>
2026-03-28 10:03:43 +0000

    Implement Lichess game collector and initialize raw data ingestion

---
+++ collectors/lichess_collector.py
@@ -0,0 +1,36 @@
+import requests
+import os
+from io import StringIO
+
+USERNAME = "yuvi2"
+MAX_GAMES = 20
+
+OUTPUT_DIR = "../data/raw_games"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+url = f"https://lichess.org/api/games/user/{USERNAME}"
+params = {
+    "max": MAX_GAMES,
+    "pgnInJson": True,
+    "opening": True,
+    "moves": True
+}
+
+headers = {
+    "Accept": "application/x-ndjson"
+}
+
+response = requests.get(url, params=params, headers=headers)
+response.raise_for_status()
+
+games = response.text.strip().split("\n")
+print(games)
+
+for idx, game_json in enumerate(games):
+    print(idx)
+    filename = os.path.join(OUTPUT_DIR, f"game_{idx}.json")
+    with open(filename, "w") as f:
+        f.write(game_json)
+        print(game_json)
+
+print(f"Saved {len(games)} games to {OUTPUT_DIR}")
